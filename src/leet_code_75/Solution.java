54afc6ef01353f854c499c7c8da4d6092db77104
danijelrt7419 <danijelrt7419@gs.viser.edu.rs>
2022-12-15 00:53:12 +0100

    added some comments and island count algo

--- src/leet_code_75/Solution.java
+++ src/leet_code_75/Solution.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 
 public class Solution {
-
+    // Running sum of 1d Array
     static int[] runningSum(int[] nums) {
         for (int i = 1; i < nums.length; i++) {
             nums[i] += nums[i - 1];
         }
         return nums;
     }
-
+    // Find Pivot Index
     static int pivotIndex(int[] nums) {
         int totalSum = 0;
         int leftsum = 0;
@@ -23,14 +23,14 @@ public class Solution {
                 return i;
         return -1;
     }
-
+    // Isomorphic Strings
     static boolean isIsomorphic(String s, String t) {
         Map<java.io.Serializable, Integer> m = new HashMap<java.io.Serializable, Integer>();
         for (Integer i = 0; i < s.length(); ++i)
             if (m.put(s.charAt(i), i) != m.put(t.charAt(i) + "", i)) return false;
         return true;
     }
-
+    // Is Subsequence
     static boolean isSubsequence(String s, String t) {
         if (s.length() == 0) return true;
         int i = 0, j = 0;
@@ -42,7 +42,7 @@ public class Solution {
         }
         return i == s.length();
     }
-
+    // Merge Two Sorted Lists
     static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         else if (l2 == null) return l1;
@@ -68,7 +68,7 @@ public class Solution {
 
         return dummy.next;
     }
-
+    // Reverse Linked List
     static ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode current = head;
@@ -81,7 +81,7 @@ public class Solution {
         }
         return prev;
     }
-
+    // Middle of the Linked List
     static ListNode middleNode(ListNode head) {
         ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
@@ -90,7 +90,7 @@ public class Solution {
         }
         return slow;
     }
-
+    // Linked List Cycle 2
     static ListNode detectCycle(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
@@ -109,7 +109,7 @@ public class Solution {
         }
         return null;
     }
-
+    // Best Time to Buy and Sell Stock
     static int maxProfit(int[] prices) {
         int lsf = Integer.MAX_VALUE;
         int op = 0;
@@ -126,7 +126,7 @@ public class Solution {
         }
         return op;
     }
-
+    // Longest Palindrome
     static int longestPalindrome(String s) {
         int[] count = new int[256];
         int odds = 0;
@@ -137,7 +137,7 @@ public class Solution {
         }
         return s.length() - odds + (odds > 0 ? 1 : 0);
     }
-
+    // N-ary Tree Preorder Traversal
     static List<Integer> preorder(Node root) {
         List<Integer> output = new ArrayList<Integer>();
 
@@ -158,7 +158,7 @@ public class Solution {
         }
         return output;
     }
-
+    // Binary Tree Level Order Traversal
     static List<List<Integer>> levelOrder(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
@@ -178,7 +178,7 @@ public class Solution {
         }
         return wrapList;
     }
-
+    // Binary Search
     static int binarySearch(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
@@ -198,7 +198,7 @@ public class Solution {
         }
         return -1;
     }
-
+    // First Bad Version
     static boolean isBadVersion(int n) {
         for (int i = 0; i <= n; i++) {
             if (i == n) return true;
@@ -223,7 +223,7 @@ public class Solution {
         return start;
     }
 
-
+    // Validate Binary Search Tree
     public boolean isValid(TreeNode root, long min, long max) {
         if (root == null) return true;
         if (root.val >= max || root.val <= min) return false;
@@ -235,25 +235,7 @@ public class Solution {
         return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    // I added memo for speed !
-    static Map<Long, Long> hmap = new HashMap<>();
-    static Long j = 1L;
-
-    static Long fib(long n) {
-        if (n == 0) {
-            return 0L;
-        }
-        if (n == 1) {
-            return 1L;
-        }
-        if (hmap.containsKey(n)) return hmap.get(n);
-
-        Long res = fib(n - 1) + fib(n - 2);
-        hmap.put(n, res);
-        return res;
-
-    }
-
+    // Flood Fill
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int[] dRow = {-1, 0, 1, 0};
         int[] dCol = {0, 1, 0, -1};
@@ -274,7 +256,7 @@ public class Solution {
                 int adjy = c + dCol[i];
                 // Check for in bounds and start color
                 // NOTE: We don't need visited list, since we check if the color is equal to the starting color. If its not then its visited already.
-                if (adjx >= 0 && adjy >= 0 && adjx < image.length && adjy < image[0].length && image[adjx][adjy] == startColor ) {
+                if (adjx >= 0 && adjy >= 0 && adjx < image.length && adjy < image[0].length && image[adjx][adjy] == startColor) {
                     image[adjx][adjy] = color;
                     queue.add(new Pair<>(adjx, adjy));
                 }
@@ -282,6 +264,58 @@ public class Solution {
 
         }
         return image;
+    }
+    // Number of Islands
+    private static boolean explore(char[][] grid, int row, int col, Set<Pair<Integer, Integer>> visited) {
+        Pair<Integer, Integer> pos = new Pair<>(row, col);
+
+        boolean rowInBounds = 0 <= row && row < grid.length;
+        boolean colInBounds = 0 <= col && col < grid[0].length;
+        if (!rowInBounds || !colInBounds) return false;
+        if (grid[row][col] == '0') return false;
+        if (visited.contains(pos)) return false;
+        visited.add(pos);
+
+        explore(grid, row - 1, col, visited);
+        explore(grid, row + 1, col, visited);
+        explore(grid, row, col - 1, visited);
+        explore(grid, row, col + 1, visited);
+
+        return true;
+    }
+
+    public int numIslands(char[][] grid) {
+
+        Set<Pair<Integer, Integer>> visited = new HashSet<>();
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (explore(grid, i, j, visited)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+    // Fibonacci Number
+    // I added memo for speed !
+    static Map<Long, Long> hmap = new HashMap<>();
+    static Long j = 1L;
+
+    static Long fib(long n) {
+        if (n == 0) {
+            return 0L;
+        }
+        if (n == 1) {
+            return 1L;
+        }
+        if (hmap.containsKey(n)) return hmap.get(n);
+
+        Long res = fib(n - 1) + fib(n - 2);
+        hmap.put(n, res);
+        return res;
+
     }
 
 
